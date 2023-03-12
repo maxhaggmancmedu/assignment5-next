2f51dcb404e4b9c34dd7ae20ff8fd6fc27891748
Max Häggman <max.haggman@edu.cmeducations.se>
2023-03-12 12:23:05 +0100

    change initial pos animation

--- app/page/about/page.js
+++ app/page/about/page.js
@@ -24,10 +24,10 @@ export default function About() {
                 <section className={styles.sectionAboutMe}>
                     <motion.div transition={{duration: 1}} initial={{scale: 0}} animate={{scale: 1 }}  className={styles.aboutMe}>My name is Max</motion.div>
                     <div className={styles.experience}>
-                        <motion.p animate={{x: 0}} initial={{x: -1000}} transition={{duration: 1.5}}>I have studied front-end development for <span className={styles.timeStudied}>{timeSinceBeginStudy}</span></motion.p>
+                        <motion.p animate={{x: 0}} initial={{x: -2000}} transition={{duration: 2}}>I have studied front-end development for <span className={styles.timeStudied}>{timeSinceBeginStudy}</span></motion.p>
                         <div>
-                            <motion.p animate={{x: 0}} initial={{x: -1000}} transition={{duration: 1.5, delay: .5}}>I am comfortable with html, css, vanilla JS and jquery!</motion.p>
-                            <motion.p animate={{x: 0}} initial={{x: -1000}} transition={{duration: 1.5, delay: 1}}>I am currently learning different JS framworks such as react and next.js!</motion.p>
+                            <motion.p animate={{x: 0}} initial={{x: -2000}} transition={{duration: 2, delay: .5}}>I am comfortable with html, css, vanilla JS and jquery!</motion.p>
+                            <motion.p animate={{x: 0}} initial={{x: -2000}} transition={{duration: 2, delay: 1}}>I am currently learning different JS framworks such as react and next.js!</motion.p>
                         </div>
                     </div>
                 </section>
